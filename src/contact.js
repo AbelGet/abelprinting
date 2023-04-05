1f4e8e4ebab821c79cc385fc9d188dd64b5c7208
Abel <85369262+AbelGet@users.noreply.github.com>
2023-04-05 15:57:42 +0300

    contact page is fixed now sending email is working so anyone can send email.

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ScrollReveal from 'scrollreveal';
 import Footer from "./footer";
 import contact from "./images/contact.png"
+import emailjs from '@emailjs/browser';
+import success from "./images/success.png";
 
 const Contact = () => {
     useEffect(() => {
@@ -14,38 +16,62 @@ const Contact = () => {
         });
         sr.reveal('.contactContanier', { interval: 100 });
     }, []);
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_lg1gjsk', 'template_uowwfns', form.current, 'TCUZuvjfYwPZR8vOw')
+        .then((result) => {
+            // console.log(result.text);
+            setTimeout(() => {
+                
+            }, 5000);
+        }, (error) => {
+            console.log(error.text);
+        });
+        
+        e.target.reset();
+    };
     return (
         <div className="contentContact">
             <div className="contactContanier">
                 <div className="form">
-                    <form action="">
+                    <form action="" ref={form} onSubmit={sendEmail}>
                         <h2 className="title">Contact Us </h2>
                         <div className="input-group">
                             <label>First Name:</label>
-                            <input type="text" placeholder="Jhon"/>
+                            <input type="text" placeholder="Jhon" name='user_fname'/>
                         </div>
 
                         <div className="input-group">
                             <label>Last Name:</label>
-                            <input type="text" placeholder="Doe"/>
+                            <input type="text" placeholder="Doe" name='user_lname'/>
                         </div>
 
                         <div className="input-group">
                             <label>Phone Number:</label>
-                            <input type="number" placeholder="+251930033807"/>
+                            <input type="number" placeholder="+251930033807" name='user_phno'/>
                         </div>
 
                         <div className="input-group">
                             <label>Email:</label>
-                            <input type="email" placeholder="abeladvert@yahoo.com" className="email-input"/>
+                            <input type="email" placeholder="abeladvert@yahoo.com" className="email-input" name='user_email'/>
                         </div>
 
                         <div className="input-group">
                             <label>Message:</label>
-                            <textarea placeholder="Message"></textarea>
+                            <textarea placeholder="Message" name='user_message'></textarea>
                         </div>
-                        <div className="input-group">
-                            <button className="btnSubmit">Submitt</button>
+                        <div className="input-group-btn">
+                            <div>
+                                <button className="btnSubmit" type='submite'>Submitt</button>
+                            </div>
+                            
+                            <div className='success'>
+                                <img src={success} alt="Success" />
+                                <label>You have successfully send email</label>
+                            </div>
                         </div>
                     </form>
                 </div>
